8a91bba5091c257d443051616570f31b9d5efa06
Li Ouyang <li.ouyang@gmail.com>
2012-11-01 15:01:19 -0400

    initial commint

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudentBodyRails::Application.config.secret_token = '76d184858b6c18cec599fe4e208d29125d8d1a8360985f181da9eb87126b0adaa9519a4c627edb559635d25e05a39bdabea6f84317b7c35c5ef9f81c37585424'
